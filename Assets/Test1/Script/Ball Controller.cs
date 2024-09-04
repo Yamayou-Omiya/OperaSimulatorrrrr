e095fcbc61a769752b151fb46a352f71cb28930b
haru-it9 <it.yanyan9@gmail.com>
2024-09-04 19:05:30 +0900

    update 'New Terrain 2.asset', TestScene1.unity and 'Ball Controller.cs'

--- Assets/Test1/Script/Ball Controller.cs
+++ Assets/Test1/Script/Ball Controller.cs
@@ -6,7 +6,7 @@ public class BallController : MonoBehaviour
 {
     public Rigidbody rigidbody;
 
-    public Vector3 ball_transform = new Vector3(0.005f, 0f, 0.005f);
+    public Vector3 ball_transform = new Vector3(0.05f, 0f, 0.05f);
     
     // Start is called before the first frame update
     void Start()
@@ -17,16 +17,16 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //this.transform.position += ball_transform;
-        rigidbody.velocity = ball_transform;
+        this.transform.position += ball_transform;
+        //rigidbody.velocity = ball_transform;
     }
 
     private void OnCollisionEnter(Collision collision){
         if(collision.gameObject.name == "Frame_R" || collision.gameObject.name == "Frame_L"){
-            ball_transform.x = -1*ball_transform.x;
+            ball_transform.x = -1 * ball_transform.x;
         }
         if(collision.gameObject.name == "Frame_U" || collision.gameObject.name == "Frame_B"){
-            ball_transform.z = -1*ball_transform.z;
+            ball_transform.z = -1 * ball_transform.z;
         }
     }
 }

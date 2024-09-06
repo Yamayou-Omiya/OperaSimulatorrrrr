c36f3de8f15df280cff869bb6315d79c801fff06
haru-it9 <it.yanyan9@gmail.com>
2024-09-06 12:12:00 +0900

    update TestScene1.unity and 'Area Controller.cs'

--- Assets/Test1/Script/Area Controller.cs
+++ Assets/Test1/Script/Area Controller.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class AreaController : MonoBehaviour
 {
-    //public Rigidbody rigidbody;
+    public Rigidbody rigidbody;
     private Vector3 mouse;
     private Vector3 area;
+    [SerializeField] GameObject target;
     
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,25 @@ public class AreaController : MonoBehaviour
     {
         mouse = Input.mousePosition;
         this.transform.position = new Vector3((mouse.x-960)/(960/5), 0, (mouse.y-540)/(540/2.8f));
-        Debug.Log("m"+mouse);
-        Debug.Log("a"+area);
+        
+        
+        
+        //Debug.Log("m"+mouse);
+        //Debug.Log("a"+area);
     }
+
+    private void Collision(){
+        if(target.transform.position.x >= this.transform.position.x - 0.5 || )
+    }
+
+    /*private void OnCollisionEnter(Collision collision){
+        //if(collision.gameObject.name == "Ball Target"){
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log(collision.gameObject.name);
+        //}
+    }*/
+
+    /*private void OnCollisionExit(Collision collision){
+        gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+    }*/
 }

82bccbf20ceb7fe4046be4520feaf5e9e95c0792
haru-it9 <it.yanyan9@gmail.com>
2024-09-09 15:07:25 +0900

    update 1 file and create 2 files

--- Assets/Test1/Script/Area Controller.cs
+++ Assets/Test1/Script/Area Controller.cs
@@ -8,6 +8,9 @@ public class AreaController : MonoBehaviour
     private Vector3 mouse;
     private Vector3 area;
     [SerializeField] GameObject target;
+    [SerializeField] bool mouse_control;
+    [SerializeField] bool key_control;
+    [SerializeField] bool controller_control;
     
     // Start is called before the first frame update
     void Start()
@@ -18,27 +21,27 @@ public class AreaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mouse = Input.mousePosition;
-        this.transform.position = new Vector3((mouse.x-960)/(960/5), 0, (mouse.y-540)/(540/2.8f));
-        
-        
-        
-        //Debug.Log("m"+mouse);
-        //Debug.Log("a"+area);
+        if(mouse_control)
+        {
+            mouse = Input.mousePosition;
+            this.transform.position = new Vector3((mouse.x - 960)/(960/5), 0, (mouse.y - 540)/(540/2.8f));
+        }
+
+        gameObject.GetComponent<Renderer>().material.color = color_changer();
+    }       
+
+    private Color color_changer(){
+        float xdistance = target.transform.position.x - this.transform.position.x;
+        float zdistance = target.transform.position.z - this.transform.position.z;
+
+        if((xdistance*xdistance + zdistance*zdistance) <= 0.25f)
+        {
+            return Color.red;
+        }
+        else
+        {
+            return Color.yellow;
+        }
     }
-
-    private void Collision(){
-        if(target.transform.position.x >= this.transform.position.x - 0.5 || )
-    }
-
-    /*private void OnCollisionEnter(Collision collision){
-        //if(collision.gameObject.name == "Ball Target"){
-            gameObject.GetComponent<Renderer>().material.color = Color.red;
-            Debug.Log(collision.gameObject.name);
-        //}
-    }*/
-
-    /*private void OnCollisionExit(Collision collision){
-        gameObject.GetComponent<Renderer>().material.color = Color.yellow;
-    }*/
+    
 }

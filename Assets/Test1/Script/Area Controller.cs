7337485ea6e903fecc37b35c91cccbfa6408f06d
haru-it9 <it.yanyan9@gmail.com>
2024-09-09 15:54:11 +0900

    update TestScene1.unity, 'Area Controller.cs', 'Ball Controller.cs' and Timer.cs

--- Assets/Test1/Script/Area Controller.cs
+++ Assets/Test1/Script/Area Controller.cs
@@ -8,14 +8,14 @@ public class AreaController : MonoBehaviour
     private Vector3 mouse;
     private Vector3 area;
     [SerializeField] GameObject target;
+    public float areaDiameter = 1.0f;
     [SerializeField] bool mouse_control;
-    [SerializeField] bool key_control;
     [SerializeField] bool controller_control;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        diameter_changer(areaDiameter);
     }
 
     // Update is called once per frame
@@ -24,7 +24,7 @@ public class AreaController : MonoBehaviour
         if(mouse_control)
         {
             mouse = Input.mousePosition;
-            this.transform.position = new Vector3((mouse.x - 960)/(960/5), 0, (mouse.y - 540)/(540/2.8f));
+            this.transform.position = new Vector3((mouse.x - 960)/(960/5.05f), 0, (mouse.y - 540)/(540/2.85f));
         }
 
         gameObject.GetComponent<Renderer>().material.color = color_changer();
@@ -34,7 +34,7 @@ public class AreaController : MonoBehaviour
         float xdistance = target.transform.position.x - this.transform.position.x;
         float zdistance = target.transform.position.z - this.transform.position.z;
 
-        if((xdistance*xdistance + zdistance*zdistance) <= 0.25f)
+        if((xdistance*xdistance + zdistance*zdistance) <= areaDiameter*areaDiameter/4)
         {
             return Color.red;
         }
@@ -43,5 +43,15 @@ public class AreaController : MonoBehaviour
             return Color.yellow;
         }
     }
-    
+
+    private void diameter_changer(float newDiameter)
+    {
+        Vector3 scale = transform.localScale;
+
+        scale.x = newDiameter;
+        scale.z = newDiameter;
+
+        transform.localScale = scale;
+    }
+
 }
--- Assets/Test1/Script/Ball Controller.cs
+++ Assets/Test1/Script/Ball Controller.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     public Rigidbody rigidbody;
-
+    public float ballDiameter = 0.2f;
     public Vector3 ball_transform = new Vector3(0.05f, 0f, 0.05f);
     
     // Start is called before the first frame update
     void Start()
     {
-        //rigidbody = this.GetComponent<Rigidbody>();
+        diameter_changer(ballDiameter);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         this.transform.position += ball_transform;
-        //rigidbody.velocity = ball_transform;
     }
 
     private void OnCollisionEnter(Collision collision){
@@ -29,4 +28,16 @@ public class BallController : MonoBehaviour
             ball_transform.z = -1 * ball_transform.z;
         }
     }
+
+    private void diameter_changer(float newDiameter)
+    {
+        Vector3 scale = transform.localScale;
+
+        scale.x = newDiameter;
+        scale.y = newDiameter;
+        scale.z = newDiameter;
+
+        transform.localScale = scale;
+    }
+
 }

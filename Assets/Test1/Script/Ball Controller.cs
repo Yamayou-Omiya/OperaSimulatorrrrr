3be49f1276ae5133fa15f773036b8fb1e69a9d48
haru-it9 <it.yanyan9@gmail.com>
2024-09-11 22:28:42 +0900

    update TestScene1.unity and 'Ball Controller.cs'

--- Assets/Test1/Script/Ball Controller.cs
+++ Assets/Test1/Script/Ball Controller.cs
@@ -7,6 +7,8 @@ public class BallController : MonoBehaviour
     public Rigidbody rigidbody;
     public float ballDiameter = 0.2f;
     public Vector3 ball_transform = new Vector3(0.05f, 0f, 0.05f);
+    public float speed_x = 0.05f;
+    public float speed_z = 0.05f; 
     
     // Start is called before the first frame update
     void Start()
@@ -22,13 +24,57 @@ public class BallController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.name == "Frame_R" || collision.gameObject.name == "Frame_L")
+        if(collision.gameObject.name == "Frame_R")
         {
-            ball_transform.x = -1 * ball_transform.x;
+            ball_transform.x = Random.Range(-1 * speed_x, -1 * speed_x / 2);
+            int dice = Random.Range(1, 3);
+            if(dice == 1)
+            {
+                ball_transform.z = Random.Range(-1 * speed_z, -1 * speed_z / 2);
+            }
+            else if(dice == 2)
+            {
+                ball_transform.z = Random.Range(speed_z, speed_z / 2);
+            }     
         }
-        if(collision.gameObject.name == "Frame_U" || collision.gameObject.name == "Frame_B")
+        if(collision.gameObject.name == "Frame_L")
         {
-            ball_transform.z = -1 * ball_transform.z;
+            ball_transform.x = Random.Range(speed_x / 2, speed_x);
+            int dice = Random.Range(1, 3);
+            if(dice == 1)
+            {
+                ball_transform.z = Random.Range(-1 * speed_z, -1 * speed_z / 2);
+            }
+            else if(dice == 2)
+            {
+                ball_transform.z = Random.Range(speed_z, speed_z / 2);
+            }     
+        }
+        if(collision.gameObject.name == "Frame_U")
+        {
+            ball_transform.z = Random.Range(-1 * speed_z, -1 * speed_z / 2);
+            int dice = Random.Range(1, 3);
+            if(dice == 1)
+            {
+                ball_transform.x = Random.Range(-1 * speed_x, -1 * speed_x / 2);
+            }
+            else if(dice == 2)
+            {
+                ball_transform.x = Random.Range(speed_x, speed_x / 2);
+            }     
+        }
+        if(collision.gameObject.name == "Frame_B")
+        {
+            ball_transform.z = Random.Range(speed_z / 2, speed_z);
+            int dice = Random.Range(1, 3);
+            if(dice == 1)
+            {
+                ball_transform.x = Random.Range(-1 * speed_x, -1 * speed_x / 2);
+            }
+            else if(dice == 2)
+            {
+                ball_transform.x = Random.Range(speed_x, speed_x / 2);
+            }     
         }
     }
 

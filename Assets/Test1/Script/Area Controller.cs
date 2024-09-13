4dda1f8f69de93ddb6d4be8ded17c03accfa84f6
haru-it9 <it.yanyan9@gmail.com>
2024-09-13 14:26:19 +0900

    update 10 files, copy 1 file and create 1 file

--- Assets/Test1/Script/Area Controller.cs
+++ Assets/Test1/Script/Area Controller.cs
@@ -6,11 +6,14 @@ public class AreaController : MonoBehaviour
 {
     public Rigidbody rigidbody;
     private Vector3 mouse;
-    private Vector3 area;
     [SerializeField] GameObject target;
+    [SerializeField] GameObject sardine;
     public float areaDiameter = 1.0f;
     [SerializeField] bool mouse_control;
     [SerializeField] bool controller_control;
+
+    private float stayTime = 0;
+    public float requiredStayTime = 3.0f
     
     // Start is called before the first frame update
     void Start()
@@ -30,13 +33,23 @@ public class AreaController : MonoBehaviour
         gameObject.GetComponent<Renderer>().material.color = color_changer();
     }       
 
-    private Color color_changer(){
-        float xdistance = target.transform.position.x - this.transform.position.x;
-        float zdistance = target.transform.position.z - this.transform.position.z;
+    private Color color_changer()
+    {
+        float xdistance = sardine.transform.position.x - this.transform.position.x;
+        float zdistance = sardine.transform.position.z - this.transform.position.z;
 
         if((xdistance*xdistance + zdistance*zdistance) <= areaDiameter*areaDiameter/4)
         {
-            return Color.red;
+            stayTime += Time.deltaTime;
+            if(stayTime >= requiredStayTime)
+            {
+                return color_changer.green;
+            }
+            else
+            {
+                return Color.red;
+            }
+            
         }
         else
         {
--- Assets/Test1/Script/Sardine Controller.cs
+++ Assets/Test1/Script/Sardine Controller.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class SardineController : MonoBehaviour
 {
     public Rigidbody rigitbody;
+    public Vector3 sardine_transform = new Vector3(0.005f, 0, 0.005f);
+    private float deg = 0;
+    public float speed_x = 0.005f;
+    public float speed_z = 0.005f; 
+
+
     
     // Start is called before the first frame update
     void Start()
@@ -15,6 +21,73 @@ public class SardineController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rotation(sardine_transform.z, sardine_transform.x);
+        this.transform.position += sardine_transform;
+    }
+
+    private void rotation(float z, float x)
+    {
+        deg = Mathf.Atan2(z, x) * Mathf.Rad2Deg;
+        if(deg > -90 && deg < 0 || deg > 90 && deg < 180)
+        {
+            deg = deg + 180;
+        }
+        this.gameObject.transform.eulerAngles = new Vector3(0, deg, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Frame_R")
+        {
+            sardine_transform.x = Random.Range(-1 * speed_x, -1 * speed_x / 2);
+            int dice = Random.Range(1, 3);
+            if(dice == 1)
+            {
+                sardine_transform.z = Random.Range(-1 * speed_z, -1 * speed_z / 2);
+            }
+            else if(dice == 2)
+            {
+                sardine_transform.z = Random.Range(speed_z, speed_z / 2);
+            }     
+        }
+        if(collision.gameObject.name == "Frame_L")
+        {
+            sardine_transform.x = Random.Range(speed_x / 2, speed_x);
+            int dice = Random.Range(1, 3);
+            if(dice == 1)
+            {
+                sardine_transform.z = Random.Range(-1 * speed_z, -1 * speed_z / 2);
+            }
+            else if(dice == 2)
+            {
+                sardine_transform.z = Random.Range(speed_z, speed_z / 2);
+            }     
+        }
+        if(collision.gameObject.name == "Frame_U")
+        {
+            sardine_transform.z = Random.Range(-1 * speed_z, -1 * speed_z / 2);
+            int dice = Random.Range(1, 3);
+            if(dice == 1)
+            {
+                sardine_transform.x = Random.Range(-1 * speed_x, -1 * speed_x / 2);
+            }
+            else if(dice == 2)
+            {
+                sardine_transform.x = Random.Range(speed_x, speed_x / 2);
+            }     
+        }
+        if(collision.gameObject.name == "Frame_B")
+        {
+            sardine_transform.z = Random.Range(speed_z / 2, speed_z);
+            int dice = Random.Range(1, 3);
+            if(dice == 1)
+            {
+                sardine_transform.x = Random.Range(-1 * speed_x, -1 * speed_x / 2);
+            }
+            else if(dice == 2)
+            {
+                sardine_transform.x = Random.Range(speed_x, speed_x / 2);
+            }     
+        }
     }
 }

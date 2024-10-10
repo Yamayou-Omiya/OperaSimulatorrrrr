494413d7ee5f39e47b6ec846892ed0d4cf699a4a
haru-it9 <it.yanyan9@gmail.com>
2024-10-10 14:35:12 +0900

    delete 2 files and update 2 files

---
+++ Assets/Test1/Script/CountDown.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField]string countDownTextName = "CountdownText";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Countdown()
+    {
+        countdownText.enabled = false;
+            
+        yield return new WaitForSeconds(1);  //1秒待つ
+
+        //文字を表示する
+        countdownText.enabled = true;
+
+        //3,2,1…とカウントダウンするループ
+        for(int i = 2; i >= 0; i--)
+        {
+            countdownText.text = (i + 1).ToString();  //文字の表示を切り替える
+            yield return new WaitForSeconds(1);  //1秒待つ
+        }
+
+        //文字を非表示にする
+        countdownText.enabled = false;
+    }
+}

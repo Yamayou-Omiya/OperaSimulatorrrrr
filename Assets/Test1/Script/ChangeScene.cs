50d402f2c0491b2b7f7a77eb78e15c72b0eb8ff5
haru-it9 <it.yanyan9@gmail.com>
2024-10-15 13:42:26 +0900

    update StartScene.unity and ChangeScene.cs

--- Assets/Test1/Script/ChangeScene.cs
+++ Assets/Test1/Script/ChangeScene.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
 {
+    public Text alertText;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,9 +26,30 @@ public class ChangeScene : MonoBehaviour
     }
     public void StartButtonClicked()
     {
-        if()
+        if(MainTaskLevel.mainLevel1||MainTaskLevel.mainLevel2||MainTaskLevel.mainLevel3||MainTaskLevel.mainLevel4||MainTaskLevel.mainLevel5)
         {
-            
+            if(SubTaskLevel.subLevel1||SubTaskLevel.subLevel2||SubTaskLevel.subLevel3)
+            {
+                if(ScreenPosition.screenPosition1||ScreenPosition.screenPosition2||ScreenPosition.screenPosition3||ScreenPosition.screenPositionFull)
+                {
+                    SceneManager.LoadScene("TestScene2");
+                }
+                else
+                {
+                    alertText.text = "Select Number!!";
+                    alertText.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+                }
+            }
+            else
+            {
+                alertText.text = "Select Number!!";
+                alertText.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+            }
+        }
+        else
+        {
+            alertText.text = "Select Number!!";
+            alertText.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
         }
     }
 }

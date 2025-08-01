e73011b246ea519f891e7a03bb19690c28e6ee4f
haruto <23haru-it57@akane.waseda.jp>
2025-08-02 00:36:52 +0900

    Refactor code structure and remove redundant changes

--- Assets/OCScene/RockCollision.cs
+++ Assets/OCScene/RockCollision.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class RockCollision : MonoBehaviour
 {
+    private bool pointAdded = false;
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("bucket"))
         {
             Debug.Log("Bucket と衝突したので削除します");
             Destroy(gameObject);  // 自分自身を削除
+
         }
     }
 }
